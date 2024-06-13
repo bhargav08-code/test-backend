f1f96663cdd5b35c2b58c056775956c35d578f5f
bhargav08-code <bhargavgarge1828@gmail.com>
2024-06-14 03:21:56 +0530

    Changes made in sever.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import cartRouter from "./Routes/cartRoute.js";
 import orderRouter from "./Routes/orderRoute.js";
 //app config
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 //middleware
 app.use(express.json());
